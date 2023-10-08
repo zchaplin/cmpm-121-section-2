dddb503da635bd643ac4e636292ba418ea6760fe
Adam Smith <adam@adamsmith.as>
2023-10-08 15:39:15 -0700

    Cleanup before section

---
+++ README.md
@@ -0,0 +1,5 @@
+# Section 2 Starter Code
+
+# Credits
+
+Image assets from https://www.spriters-resource.com/browser_games/googledinosaurrungame/sheet/78171/
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig ({
-    plugins: [vue()],
-    base: "/project_2_vite"
+    base: "/cmpm-121-section-2"
 })
