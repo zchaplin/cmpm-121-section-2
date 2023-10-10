cb753f85ab31ac7989099519037b56016ff5863c
zchaplin <zchaplin@ucsc.edu>
2023-10-09 18:07:36 -0700

    Done with first partner

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,15 @@ let score = 0
 SetText("click to start!")
 
 
-var isJumping = false
+let isJumping = false
 let gameOver = true
 
 document.addEventListener('mousedown', () => jump())
 
-setInterval(function () { Main()}, 10)
+setInterval(function () { Main() }, 10)
 
-function Main()
-{
-    if(gameOver == false)
-    {
+function Main() {
+    if (gameOver == false) {
         score = score + 1;
         SetText("Score: " + score)
 
@@ -30,64 +28,59 @@ function Main()
 }
 
 
-function jump()
-{
-    if(gameOver == false && isJumping == false)
-    {
+function jump() {
+    if (gameOver == false && isJumping == false) {
         isJumping = true
         dino?.classList.add("jump")
         setTimeout(RemoveJump, 500)
     }
-    else if (gameOver == true){
+    else if (gameOver == true) {
         StartGame();
     }
-    
+
 }
 
 
-function RemoveJump()
-{
+function RemoveJump() {
     dino?.classList.remove("jump")
     isJumping = false;
     //mainLoop = mainLoop //bug fix?
 }
 
-function RemoveObstacles()
-{
+function RemoveObstacles() {
     cactus?.classList.remove("cactusMove")
     bird?.classList.remove("birdMove")
 }
+function getProperty(obj: HTMLElement, str: string){
+    return parseInt(window.getComputedStyle(obj).getPropertyValue(str))
+}
 
 
-function CheckGameOver()
-{
+function CheckGameOver() {
 
-    if(gameOver == false && dino != null && cactus != null && bird != null)
-    {
+    if (gameOver == false && dino != null && cactus != null && bird != null) {
         //get is dinosaur jumping
-        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+        let dinoTop = getProperty(dino,"top")
 
         //get cactus position
-        let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+        let cactusleft = getProperty(cactus,"left")
 
         //get bird position
-        let birdleft = parseInt(window.getComputedStyle(bird).getPropertyValue("left"))
+        let birdleft = getProperty(bird,"left")
 
         //detect cactus collision
-        if(dinoTop >= 150 && Math.abs(cactusleft) < 7)
-        {
+        if (dinoTop >= 150 && Math.abs(cactusleft) < 7) {
             endGame()
         }
 
         //detect bird collision
-        if(dinoTop <= 55 && Math.abs(birdleft) < 11)
-        {
+        if (dinoTop <= 55 && Math.abs(birdleft) < 11) {
             //end game
             endGame()
         }
     }
 }
-function endGame(){
+function endGame() {
     //end game
     console.log("player died!")
     SetText("Final Score: " + score + "! Click To Play Again!")
@@ -95,13 +88,12 @@ function endGame(){
 
     //reset player
     RemoveJump()
-    
+
     //reset cactus
     RemoveObstacles()
 }
 
-function StartGame()
-{
+function StartGame() {
     console.log("Game started!")
     gameOver = false
     score = 0
@@ -109,10 +101,8 @@ function StartGame()
     bird?.classList.add("birdMove")
 }
 
-function SetText(s: string)
-{
-    if(scoreText)
-    {
+function SetText(s: string) {
+    if (scoreText) {
         scoreText.textContent = s
     }
 }
